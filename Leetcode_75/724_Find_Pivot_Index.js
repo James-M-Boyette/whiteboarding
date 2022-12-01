559542027297c3dfd73fa0b3752739c540778b26
sou7hernsaint <inquiries@james-boyette.com>
2022-12-01 11:12:59 -0500

    chore: finish problem #2

--- Leetcode_75/724_Find_Pivot_Index.js
+++ Leetcode_75/724_Find_Pivot_Index.js
@@ -5,20 +5,50 @@
   * If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
 
   * So ...
-  * 
+  * We're taking in an array of numbers,
+  * and we want to find the number (and its index) where all numbers left of it equal the sum of one or more numbers to the right (not necessarily *all* numbers to the right)
+  * I'm not sure what the second note about "left-most, zero" etc means ...
   * 
  */
 
-//  var pivotIndex = function(nums) {
+// This works the way I thought it should - evaluating numbers on the right *up to* all of them
+// var pivotIndex = function (nums) {
+//   let leftSum = 0;
+//   let rightSum = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     for (const el of nums.slice(i + 1)) {
+//       rightSum += el;
+//       if (leftSum == rightSum) {
+//         return i;
+//       }
+//     }
+//     leftSum += nums[i];
+//     rightSum = 0;
+//   }
+//   return -1;
+// };
+
+// This works the way the problem is described
 var pivotIndex = function (nums) {
-  return;
+  let leftSum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    const rightSum = nums.slice(i + 1).reduce((acc, cur) => {
+      return acc + cur;
+    }, 0);
+    if (leftSum == rightSum) {
+      return i;
+    }
+    leftSum += nums[i];
+  }
+  return -1;
 };
 
 const nums1 = [1, 7, 3, 6, 5, 6];
 const nums2 = [1, 2, 3];
 const nums3 = [2, 1, -1];
+const nums4 = [-1, -1, -1, -1, -1, -1];
 
-console.log(pivotIndex(nums1));
+console.log(pivotIndex(nums3));
 
 /** Example 1 -
  * Input: nums = [1,7,3,6,5,6]
